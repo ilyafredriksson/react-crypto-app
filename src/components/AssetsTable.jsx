a5756022dfd3035fdadc2a2e0e904877473052d4
ilyafredriksson <fredrikssonilya@gmail.com>
2025-10-11 18:45:15 +0200

    AssetsTable

--- src/components/AssetsTable.jsx
+++ src/components/AssetsTable.jsx
@@ -1,3 +1,41 @@
-export default function AssetsTable() { 
-  return <div>11111</div>;
+import { Table } from "antd";
+import { useCrypto } from "../context/crypto-context";
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    
+    sortDirections: ["descend"],
+  },
+  {
+    title: "Price,$",
+    dataIndex: "price",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: "Amount",
+    dataIndex: "amount",
+    
+  },
+];
+
+
+
+export default function AssetsTable() {
+
+const {assets} =useCrypto();
+const data =assets.map((a)=>({
+  key:a.id,
+  name:a.name||a.id,
+  price:a.price,
+  amount:a.amount,
+}));
+
+  return <Table 
+  columns={columns}
+   dataSource={data}
+    
+    pagination={false}
+     />;
 }
